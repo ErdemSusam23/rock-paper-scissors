efa6a172344f5a2c26f105c0ae60cd42c7222d38
ErdemSusam23 <ersusam2003@gmail.com>
2023-10-17 17:14:51 +0300

    fix game buttons

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let playerScore = 0;
 let computerScore = 0;
-let playerSelection;
-let computerSelection;
+let playerSelection = '';
+let winnerOfTheRound = '';
 
 function getComputerChoice() {
+	let computerSelection;
 	let dice;
 	dice = Math.floor(Math.random(1) * 3 + 1);
 
@@ -14,13 +15,15 @@ function getComputerChoice() {
 	} else if (dice == 3) {
 		computerSelection = 'Scissors';
 	}
-
-	console.log(computerSelection);
 	return computerSelection;
 }
 
 function getPlayerChoice() {
 	return playerSelection;
+}
+
+function setPlayerChoice(selection) {
+	playerSelection = selection;
 }
 
 function playRound(player, computer) {
@@ -30,9 +33,11 @@ function playRound(player, computer) {
 				console.log('Draw');
 			} else if (player == 'Paper') {
 				console.log('Players Wins');
+				winner = 'Player';
 				playerScore++;
 			} else if (player == 'Scissors') {
 				console.log('Computer Wins');
+				winner = 'Computer';
 				computerScore++;
 			}
 			break;
@@ -40,11 +45,13 @@ function playRound(player, computer) {
 		case 'Paper':
 			if (player == 'Rock') {
 				console.log('Computer Wins');
+				winner = 'Computer';
 				computerScore++;
 			} else if (player == 'Paper') {
 				console.log('Draw');
 			} else if (player == 'Scissors') {
 				console.log('Players Wins');
+				winner = 'Player';
 				playerScore++;
 			}
 			break;
@@ -52,37 +59,60 @@ function playRound(player, computer) {
 		case 'Scissors':
 			if (player == 'Rock') {
 				console.log('Players Wins');
+				winner = 'Player';
 				playerScore++;
 			} else if (player == 'Paper') {
 				console.log('Computer Wins');
+				winner = 'Computer';
 				computerScore++;
 			} else if (player == 'Scissors') {
 				console.log('Draw');
 			}
 			break;
+		default:
+			break;
 	}
 }
 
 function game() {
-	while (playerScore < 5 && computerScore < 5 && playerSelection != null) {
+	if (playerScore < 5 && computerScore < 5) {
 		playRound(getPlayerChoice(), getComputerChoice());
-		playerSelection = null;
-		alert('Player: ' + playerScore, 'Computer: ' + computerScore);
+		if (winner === '') {
+			winner = 'Draw';
+			roundWinnerText.textContent = "It's a " + winner;
+		} else {
+			gameInfoText.textContent =
+				'Player: ' + playerScore + ' Computer: ' + computerScore;
+			roundWinnerText.textContent = winner + ' Won the round';
+		}
+
+		console.log('Player: ' + playerScore + ' Computer: ' + computerScore);
+		winner = '';
+	}
+	if (computerScore == 5 || playerScore == 5) {
+		if (computerScore > playerScore) {
+			roundWinnerText.textContent = 'COMPUTER HAS WON THE GAME!';
+		} else {
+			roundWinnerText.textContent = 'PLAYER HAS WON THE GAME!';
+		}
 	}
 }
 
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
-const scissors = document.querySelector('scissors');
+const scissors = document.querySelector('#scissors');
+const gameInfoText = document.querySelector('#gameInfoText');
+const roundWinnerText = document.querySelector('#winner');
 
 rock.addEventListener('click', function () {
-	playerSelection = 'Rock';
+	setPlayerChoice('Rock');
+	game();
 });
 paper.addEventListener('click', function () {
-	playerSelection = 'Paper';
+	setPlayerChoice('Paper');
+	game();
 });
 scissors.addEventListener('click', function () {
-	playerSelection = 'Scissors';
+	setPlayerChoice('Scissors');
+	game();
 });
-
-game();
